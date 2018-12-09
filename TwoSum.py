3a1cabc327d39877cb153e75364f3a657049cc13
Burgessz <43734331+Burgessz@users.noreply.github.com>
2018-12-09 15:03:38 +0800

    Add files via upload

---
+++ TwoSum.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  9 14:30:16 2018
+
+@author: Administrator
+"""
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i in range(len(nums)):
+            if nums[i] in d:
+                return [d[nums[i]], i]
+            else:
+                d[target - nums[i]] = i
+                
+                
